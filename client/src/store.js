47834c2a75318a06527a55c8a6e73f89714ee6dc
JContreras <jacksoncontreras4@gmail.com>
2020-05-22 20:50:24 -0700

    client frontend, start redux

---
+++ client/src/store.js
@@ -0,0 +1,16 @@
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleWare = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleWare))
+);
+
+export default store;
